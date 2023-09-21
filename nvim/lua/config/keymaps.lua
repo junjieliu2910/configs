83787ab68b192e116c93155b915af9796eff6768
junjieliu <junjieliu2910@gmail.com>
2023-09-21 20:30:59 +0800

    update keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -23,8 +23,6 @@ keymap.set("n", "<space>l", ":bnext<CR>")
 keymap.set("n", "<space>h", ":bprevious<CR>")
 
 
--- Telescope 
--- keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, {})
--- keymap.set("n", "<leader>fb", require("telescope.builtin").buffers, {})
--- keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, {})
--- keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags, {})
+-- Move in wrap line
+keymap.set("n", "j", "gj")
+keymap.set("n", "k", "gk")
